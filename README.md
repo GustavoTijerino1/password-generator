8666b2b6815cef22608e4e492a3f90e1ea50b35d
Gustavo Tijerino <busg26@gmail.com>
2022-03-05 03:49:23 -0600

    I updated Js.

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ https://www.w3schools.com/jsref/met_win_confirm.asp
 https://www.w3schools.com/js/js_loop_while.asp
 
 https://www.w3schools.com/jsref/jsref_tostring_string.asp
+
+https://stackoverflow.com/questions/27936267/how-to-hide-an-element-depending-on-a-boolean-variable
--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Provided by instructor
-var allowUppercase;
-var allowLowerCase;
-var allowNumbers;
-var allowSpecials;
-var passwordCharacterCount;
-
-var uppercaseChars = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
-
 
 var generateBtn = document.querySelector("#generate");
-
 
 var allowUppercase;
 var allowLowerCase;
@@ -37,51 +24,40 @@ function askAboutCharacterLength(){
 }
 
 function generatePassword(){
-  var characterType = [];
+  var mergelist = [];
   var finalResult = "";
-  var chars =[ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g",0, 1, 2, 3, 4, 5, 6, 7, 8, 9,"!", "@", "#", "$", "%" ];
+  var upper =[ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lower =["a", "b", "c", "d", "e", "f", "g"];
+  var numbers =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var symbols =["!", "@", "#", "$", "%" ];
 
- askAboutCharacterLength()
+
+ passwordCharacterCount =askAboutCharacterLength()
 
   while(true) {
    allowUppercase = confirm("Are Upper Case Allowed?");
 
   if (allowUppercase) {
-    characterType.push(chars.join(""));
+    mergelist.push(upper.join(""));
    } 
  
   allowLowerCase = confirm("Are Lower Case Allowed?");
     if (allowLowerCase) {
-      characterType.push(chars.join(""));
+      mergelist.push(lower.join(""));
     }
 
   allowNumbers = confirm("Are Numbers Allowed?");
     if (allowNumbers){
-      characterType.push(chars.join(""));
+      mergelist.push(numbers.join(""));
     }
 
   allowSpecials = confirm("Are Special Characters Allowed?");
     if (allowSpecials){
-      characterType.push(chars.join(""));
+      mergelist.push(symbols.join(""));
     }
 
-    
-    if ( allowUppercase  && allowLowerCase|| allowNumbers || allowSpecials) {
-      if (allowUppercase  || allowLowerCase && allowNumbers || allowSpecials) {
-        if (allowUppercase  || allowLowerCase ||allowNumbers && allowSpecials){
-          if (allowUppercase  || allowLowerCase &&allowNumbers && allowSpecials){
-            if (allowUppercase  && allowLowerCase|| allowNumbers && allowSpecials){
-              if (allowUppercase  && allowLowerCase &&allowNumbers || allowSpecials){
-                if (allowUppercase  || allowLowerCase ||allowNumbers || allowSpecials){
-                  
-                }
-              }
-
-            }
-          }
-        }
-
-      }
+    // Prevent user from not selecting character type
+    if ( allowUppercase  || allowLowerCase|| allowNumbers || allowSpecials) {
       break;
     } else  {
       alert ("Please choose a character type.")
@@ -89,12 +65,13 @@ function generatePassword(){
 
   } 
 
-    var newString = chars;
-
-    
+  console.log(mergelist)
   for (var i=1; i<=passwordCharacterCount; i++ ) {
-    var randomChar = Math.floor(Math.random() * newString.length)
-    finalResult += newString[randomChar];
+    var randomSet = Math.floor(Math.random() * mergelist.length)
+    var randomString = mergelist[randomSet]
+    console.log(randomString)
+    var randomChar = Math.floor(Math.random() * randomString.length)
+    finalResult += randomString[randomChar];
     
   }
 
@@ -113,43 +90,3 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-
-
-
-// Provided by instructor
-function askAboutUppercase(){
-  allowUppercase = confirm("Are upper case characters allowed?");
-  console.log(allowUppercase);
-}
-
-// Repeat for other criteria; for the number of characters, use a prompt statement. Google it. NOTE: prompt statements think any value you provide is a string. You will need to convert it to a number.
-
-
-// Once all the criteria are determined, this function will generate the password. You can create other functions also if you need them.
-function generatePassword(){
-  var finalResult = ""
-
-  // HINT: Remember that a for-loop can iterate from a starting number to a ending number, such as the number of characters in a password.
-
-  // HINT: You may want to look into merging arrays together
-
-
-  return finalResult;
-}
-
-// Write password to the #password input
-function writePassword() {
-
-  // ask the questions first
-  askAboutUppercase();
-
-
-
-
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
